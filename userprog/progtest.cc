c165970e0cba6a4f19053b2182a0c02368edbec9
qingzt <116171689+qingzt@users.noreply.github.com>
2024-04-16 15:48:46 +0000

    Init

---
+++ userprog/progtest.cc
@@ -0,0 +1,86 @@
+// progtest.cc 
+//	Test routines for demonstrating that Nachos can load
+//	a user program and execute it.  
+//
+//	Also, routines for testing the Console hardware device.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+#include "console.h"
+#include "addrspace.h"
+#include "synch.h"
+
+//----------------------------------------------------------------------
+// StartProcess
+// 	Run a user program.  Open the executable, load it into
+//	memory, and jump to it.
+//----------------------------------------------------------------------
+
+void
+StartProcess(char *filename)
+{
+    OpenFile *executable = fileSystem->Open(filename);//打开可执行文件
+    AddrSpace *space;
+
+    if (executable == NULL) {
+	printf("Unable to open file %s\n", filename);
+	return;
+    }
+    space = new AddrSpace(executable);//创建地址空间，此时完成了页表分配，代码段和数据段被读入了内存中
+    currentThread->space = space;//当前运行的用户程序
+    space->Print();
+    delete executable;			// close file
+
+    space->InitRegisters();		// set the initial register values 设置初始的寄存器值
+    space->RestoreState();		// load page table register 加载页表寄存器
+
+    machine->Run();			// jump to the user progam 运行用户程序
+    ASSERT(FALSE);			// machine->Run never returns;
+					// the address space exits
+					// by doing the syscall "exit"
+}
+
+
+
+// Data structures needed for the console test.  Threads making
+// I/O requests wait on a Semaphore to delay until the I/O completes.
+
+static Console *console;
+static Semaphore *readAvail;
+static Semaphore *writeDone;
+
+//----------------------------------------------------------------------
+// ConsoleInterruptHandlers
+// 	Wake up the thread that requested the I/O.
+//----------------------------------------------------------------------
+
+static void ReadAvail(_int arg) { readAvail->V(); }
+static void WriteDone(_int arg) { writeDone->V(); }
+
+//----------------------------------------------------------------------
+// ConsoleTest
+// 	Test the console by echoing characters typed at the input onto
+//	the output.  Stop when the user types a 'q'.
+//----------------------------------------------------------------------
+
+void 
+ConsoleTest (char *in, char *out)
+{
+    char ch;
+
+    console = new Console(in, out, ReadAvail, WriteDone, 0);
+    readAvail = new Semaphore("read avail", 0);
+    writeDone = new Semaphore("write done", 0);
+    
+    for (;;) {
+	readAvail->P();		// wait for character to arrive
+	ch = console->GetChar();
+	console->PutChar(ch);	// echo it!
+	writeDone->P() ;        // wait for write to finish
+	if (ch == 'q') return;  // if q, quit
+    }
+}
